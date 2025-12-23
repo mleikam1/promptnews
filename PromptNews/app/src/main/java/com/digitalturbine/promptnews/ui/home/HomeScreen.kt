e7f7da47784c88865c76016b7dd21684277fbd04
mleikam1 <53997462+mleikam1@users.noreply.github.com>
2025-12-22 22:44:47 -0600

    Fix missing clickable import in home screen

--- PromptNews/app/src/main/java/com/digitalturbine/promptnews/ui/home/HomeScreen.kt
+++ PromptNews/app/src/main/java/com/digitalturbine/promptnews/ui/home/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.digitalturbine.promptnews.ui.home
 import android.content.Intent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -16,7 +17,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
