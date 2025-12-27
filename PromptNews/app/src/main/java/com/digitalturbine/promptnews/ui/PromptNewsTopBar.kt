f1570c19435e91ce6419962c9f184b810844661c
mleikam1 <53997462+mleikam1@users.noreply.github.com>
2025-12-27 10:18:12 -0600

    Fix top bar insets and height

--- PromptNews/app/src/main/java/com/digitalturbine/promptnews/ui/PromptNewsTopBar.kt
+++ PromptNews/app/src/main/java/com/digitalturbine/promptnews/ui/PromptNewsTopBar.kt
@@ -1,6 +1,7 @@
 package com.digitalturbine.promptnews.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,7 +14,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -30,8 +30,8 @@ fun PromptNewsTopBar(
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .windowInsetsPadding(TopAppBarDefaults.windowInsets)
-                .height(TopAppBarDefaults.SmallTopAppBarHeight)
+                .windowInsetsPadding(WindowInsets.statusBars)
+                .height(56.dp)
                 .padding(horizontal = 8.dp)
         ) {
             if (showBack) {
